bca4a97c79bda8043281e94381682c11b19cbe15
Gene Doyel <gene.doyel@cph.org>
2013-04-08 10:11:35 -0500

    Initial PDFtk commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Examples
+#
+#  include pdftk
+
+class pdftk {
+  $version = '1.45'
+
+  package { 'PDFtk':
+    provider => 'appdmg',
+    source   => "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-${version}-mac_osx-10.6-setup.pkg",
+  }
 }
