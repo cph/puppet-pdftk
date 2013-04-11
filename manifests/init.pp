2a7f8cee5d1ccad6779244a53cb25ec1f8e8eb66
Gene Doyel <gene.doyel@cph.org>
2013-04-11 11:07:35 -0500

    [improvement] Downgrade to 1.44 for the dmg!

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,10 @@
 #  include pdftk
 
 class pdftk {
-  $version = '1.45'
+  $version = '1.44'
 
   package { 'PDFtk':
     provider => 'appdmg',
-    source   => "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-${version}-mac_osx-10.6-setup.pkg",
+    source   => "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-${version}-osx10.6.dmg",
   }
 }
--- spec/classes/pdftk_spec.rb
+++ spec/classes/pdftk_spec.rb
@@ -4,7 +4,7 @@ describe 'pdftk' do
   it do
     should contain_package('PDFtk').with({
       :provider => "appdmg",
-      :source   => "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-1.45-mac_osx-10.6-setup.pkg",
+      :source   => "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-1.44-osx10.6.dmg",
     })
   end
 end
