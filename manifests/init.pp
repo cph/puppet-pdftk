ece9a5bf2425de2f2f45fbcb1882d49a69a6c01e
Bob Lail <bob.lail@cph.org>
2016-10-14 15:57:34 -0500

    Updated server

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@ class pdftk {
 
   package { 'PDFtk':
     provider => 'pkgdmg',
-    source   => "https://flexpaper-desktop-publisher.googlecode.com/files/pdftk_server-2.02-mac_osx-10.6-setup.pkg",
+    source   => "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.6-setup.pkg",
   }
 }
